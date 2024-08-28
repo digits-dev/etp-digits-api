f13a735f8e8c7ae8d0e9d9968e7c3b97639d7b6f
Mike Rodelas <mikerodelas@digits.ph>
2024-08-28 11:14:10 +0800

    create item master endpoint for new created item

---
+++ app/Http/Controllers/ItemMasterController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ItemMaster;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class ItemMasterController extends Controller
+{
+    public function getNewItems(Request $request){
+        try{
+            $request->validate([
+                'datefrom' => ['required', 'date_format:Y-m-d H:i:s', 'before:dateto'],
+                'dateto'   => ['required', 'date_format:Y-m-d H:i:s', 'after:datefrom'],
+            ], [
+                'datefrom.before' => 'The datefrom must be before the dateto.',
+                'dateto.after'    => 'The dateto must be after the datefrom.',
+            ]);
+
+            // Proceed with the logic if validation passes
+            $items = [];
+            $items = ItemMaster::getItems()
+                ->whereBetween('item_masters.approved_at', [$request->datefrom, $request->dateto])
+                ->orderBy('item_masters.digits_code','ASC')->paginate(50);
+
+            return response()->json([
+                'api_status' => 1,
+                'api_message' => 'success',
+                'records' => $items,
+                'http_status' => 200
+            ],200);
+
+        }
+        catch(ValidationException $ex){
+            return response()->json([
+                'api_status' => 0,
+                'api_message' => 'Validation failed',
+                'errors' => $ex->errors(),
+                'http_status' => 401
+            ], 401);
+        }
+    }
+
+    public function getUpdatedItems(Request $request){
+        try{
+            $request->validate([
+                'datefrom' => ['required', 'date_format:Y-m-d H:i:s', 'before:dateto'],
+                'dateto'   => ['required', 'date_format:Y-m-d H:i:s', 'after:datefrom'],
+            ], [
+                'datefrom.before' => 'The datefrom must be before the dateto.',
+                'dateto.after'    => 'The dateto must be after the datefrom.',
+            ]);
+
+            // Proceed with the logic if validation passes
+            $items = [];
+            $items = ItemMaster::getItems()
+                ->whereBetween('item_masters.updated_at', [$request->datefrom, $request->dateto])
+                ->orderBy('item_masters.digits_code','ASC')->paginate(50);
+
+            return response()->json([
+                'api_status' => 1,
+                'api_message' => 'success',
+                'data' => $items,
+                'http_status' => 200
+            ],200);
+
+        }
+        catch(ValidationException $ex){
+            return response()->json([
+                'api_status' => 0,
+                'api_message' => 'Validation failed',
+                'errors' => $ex->errors(),
+                'http_status' => 401
+            ], 401);
+        }
+    }
+}
---
+++ app/Models/ItemMaster.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ItemMaster extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'dimfs';
+    protected $table = 'item_masters';
+
+    public function scopeGetItems($query){
+        return $query->leftjoin('brands','item_masters.brands_id','brands.id')
+            ->leftjoin('categories','item_masters.categories_id','categories.id')
+            ->leftjoin('sku_statuses','item_masters.sku_statuses_id','sku_statuses.id')
+            ->leftjoin('uoms','item_masters.uoms_id','uoms.id')
+            ->leftjoin('vendors','item_masters.vendors_id','vendors.id')
+            ->leftjoin('subclasses','item_masters.brands_id','brands.id')
+            ->leftjoin('item_models','item_masters.brands_id','brands.id')
+            ->whereNotNull('item_masters.digits_code')
+            ->whereNotNull('item_masters.approved_at')
+            ->select(
+                'item_masters.digits_code',
+                'item_masters.item_description',
+                'item_masters.current_srp',
+                'categories.category_description as category',
+                'item_masters.categories_id',
+                'sku_statuses.sku_status_description as sku_status',
+                'item_masters.sku_statuses_id',
+                'uoms.uom_code as uom',
+                'brands.brand_description as brand',
+                'item_masters.brands_id',
+                'item_masters.model',
+                'item_masters.compatibility',
+                'subclasses.subclass_description as subclass',
+                'item_masters.subclasses_id',
+                'vendors.vendor_name as vendor',
+                'item_masters.vendors_id',
+                'item_masters.has_serial',
+                'item_models.id as models_id'
+            );
+    }
+}
