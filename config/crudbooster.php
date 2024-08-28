890bd7a3f30459e26f063e40a27c20760d897a94
Mike Rodelas <mikerodelas@digits.ph>
2024-08-28 10:11:50 +0800

    install log-viewer

--- config/crudbooster.php
+++ config/crudbooster.php
@@ -8,21 +8,23 @@ return [
         To Allowed Specific User Agent Only
         E.g : ['Android','OkHttp','Mozilla','Mac']
     */
-    'API_USER_AGENT_ALLOWED' => [],
+    'API_USER_AGENT_ALLOWED' => [
+        'Mozilla'
+    ],
 
     'USER_TABLE' => 'cms_users',
 
-    'IMAGE_FIELDS_CANDIDATE' => 'image,picture,photo,photos,foto,gambar,thumbnail',
+    'IMAGE_FIELDS_CANDIDATE' => 'image,picture,photo,photos,thumbnail',
 
-    'PASSWORD_FIELDS_CANDIDATE' => 'password,pass,pwd,passwrd,sandi,pin',
+    'PASSWORD_FIELDS_CANDIDATE' => 'password,pass,pwd,passwrd,pin',
 
-    'DATE_FIELDS_CANDIDATE' => 'date,tanggal,tgl,created_at,updated_at,deleted_at',
+    'DATE_FIELDS_CANDIDATE' => 'date,created_at,updated_at,deleted_at',
 
     'EMAIL_FIELDS_CANDIDATE' => 'email,mail,email_address',
 
-    'PHONE_FIELDS_CANDIDATE' => 'phone,phonenumber,phone_number,telp,hp,no_hp,no_telp',
+    'PHONE_FIELDS_CANDIDATE' => 'phone,phonenumber,phone_number',
 
-    'NAME_FIELDS_CANDIDATE' => 'name,nama,person_name,person,fullname,full_name,nickname,nick,nick_name,title,judul,content',
+    'NAME_FIELDS_CANDIDATE' => 'name,nama,person_name,person,fullname,full_name,nickname,nick,nick_name,title,content',
 
     'URL_FIELDS_CANDIDATE' => 'url,link',
 
