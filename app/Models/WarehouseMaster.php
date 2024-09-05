972189aade2469e1ed79057475e052dad0bb27a0
Mike Rodelas <mikerodelas@digits.ph>
2024-09-05 13:01:25 +0800

    adjust warehouse endpoint, add city

--- app/Models/WarehouseMaster.php
+++ app/Models/WarehouseMaster.php
@@ -14,7 +14,8 @@ class WarehouseMaster extends Model
     protected $table = 'customer';
 
     public function scopeGetWarehouse($query){
-        return $query->whereNotNull('customer.warehouse_name')
+        return $query->leftJoin('cities','customer.city_id','cities.id')
+            ->whereNotNull('customer.warehouse_name')
             ->whereNull('customer.close_date')
             ->whereIn('customer.channel_code_id',['RTL','FRA'])
             ->select(
@@ -24,6 +25,7 @@ class WarehouseMaster extends Model
                 'customer.building_no as address1',
                 'customer.lot_blk_no_streetname as address2',
                 'customer.barangay as address3',
+                'cities.city_name as address4',
                 'customer.area_code_zip_code as postal_code',
                 'customer.city_id as city_code',
                 'customer.state_id as state_code'
