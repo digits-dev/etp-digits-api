04f831b5e79b171e14487b8fba5afad88fb6829a
Mike Rodelas <mikerodelas@digits.ph>
2024-09-07 00:08:14 +0800

    create order status submaster

---
+++ database/migrations/2024_09_06_230924_create_order_statuses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_status',50)->nullable();
+            $table->string('status',10)->default('ACTIVE')->nullable();
+            $table->unsignedInteger('created_by')->length(10)->nullable();
+            $table->unsignedInteger('updated_by')->length(10)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_statuses');
+    }
+}
