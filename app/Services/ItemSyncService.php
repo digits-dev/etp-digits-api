7c9e03968b9f40013b099e1b206f8c014cfe0b21
Mike Rodelas <mikerodelas@digits.ph>
2024-09-17 18:40:16 +0800

    add item sync

---
+++ app/Services/ItemSyncService.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Item;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class ItemSyncService
+{
+    private function getApiData($url, $parameters=[]){
+        $secretKey = config('item-api.secret_key');
+        $uniqueString = time();
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        if($userAgent == '' || is_null($userAgent)){
+            $userAgent = config('item-api.user_agent');
+        }
+        $xAuthorizationToken = md5( $secretKey . $uniqueString . $userAgent);
+        $xAuthorizationTime = $uniqueString;
+
+        $apiItems = Http::withHeaders([
+            'X-Authorization-Token' => $xAuthorizationToken,
+            'X-Authorization-Time' => $xAuthorizationTime,
+            'User-Agent' => $userAgent
+        ])->get($url,[
+            'page' => 1,
+			'limit' => 1000,
+            'datefrom' => $parameters['datefrom'].' 00:00:00',
+            'dateto' => $parameters['dateto'].' 23:59:59'
+        ]);
+
+        return json_decode($apiItems->body(), true);
+    }
+
+    public function syncNewItems(Request $request){
+        $validation = Validator::make($request->all(), [
+            'datefrom' => ['required', 'date_format:Y-m-d', 'before:dateto'],
+            'dateto'   => ['required', 'date_format:Y-m-d', 'after:datefrom'],
+        ], [
+            'datefrom.before' => 'The datefrom must be before the dateto.',
+            'dateto.after'    => 'The dateto must be after the datefrom.',
+        ]);
+
+        if($validation->fails()){
+            // return redirect()->back()->with([
+            //     'message_type'=>"danger",
+            //     'message'=> $validation->getMessageBag()
+            // ]);
+            return response()->json([
+                'status' => 'error',
+                'message' => $validation->getMessageBag()
+            ], 402);
+        }
+        //pull new items from api
+        $newItems = $this->getApiData(config('item-api.api_create_item_url'), [
+            'datefrom' => $request->datefrom,
+            'dateto'   => $request->dateto,
+        ]);
+
+        foreach ($newItems['data'] ?? [] as $key => $value) {
+            DB::beginTransaction();
+            try {
+                Item::firstOrCreate(['digits_code'=>$value['digits_code']], $value);
+                DB::commit();
+            } catch (Exception $e) {
+                DB::rollBack();
+                Log::error($e->getMessage());
+            }
+        }
+
+        Log::info("Pull new items done!");
+        // return redirect()->back()->with([
+        //     'message' => 'Pull new items done!',
+        //     'message_type' => 'info'
+        // ]);
+        return response()->json([
+            'status' => 'success',
+            'message' => "Pull new items done!"
+        ], 200);
+    }
+
+    public function syncUpdatedItems(Request $request){
+        $validation = Validator::make($request->all(), [
+            'datefrom' => ['required', 'date_format:Y-m-d', 'before:dateto'],
+            'dateto'   => ['required', 'date_format:Y-m-d', 'after:datefrom'],
+        ], [
+            'datefrom.before' => 'The datefrom must be before the dateto.',
+            'dateto.after'    => 'The dateto must be after the datefrom.',
+        ]);
+
+        if($validation->fails()){
+            // return redirect()->back()->with([
+            //     'message_type'=>"danger",
+            //     'message'=> $validation->getMessageBag()
+            // ]);
+            return response()->json([
+                'status' => 'error',
+                'message' => $validation->getMessageBag()
+            ], 402);
+        }
+        //pull updated items from api
+        $updatedItems = $this->getApiData(config('item-api.api_update_item_url'), [
+            'datefrom' => $request->datefrom,
+            'dateto'   => $request->dateto,
+        ]);
+
+        foreach ($updatedItems['data'] ?? [] as $key => $value) {
+            DB::beginTransaction();
+            try {
+                Item::where('digits_code', $value->digits_code)->update($value);
+                DB::commit();
+            } catch (Exception $e) {
+                DB::rollBack();
+                Log::error($e->getMessage());
+            }
+        }
+
+        Log::info("Pull new items done!");
+        // return redirect()->back()->with([
+        //     'message' => 'Pull updated items done!',
+        //     'message_type' => 'info'
+        // ]);
+        return response()->json([
+            'status' => 'success',
+            'message' => "Pull updated items done!"
+        ], 200);
+    }
+}
