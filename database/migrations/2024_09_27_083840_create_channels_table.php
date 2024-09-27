1033674b6b2ce3a00c77ac7a1fb8fee31fae23dc
Mike Rodelas <mikerodelas@digits.ph>
2024-09-27 08:46:45 +0800

    create channel model

---
+++ database/migrations/2024_09_27_083840_create_channels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->id();
+            $table->string('channel_code',5)->nullable();
+            $table->string('channel_description',20)->nullable();
+            $table->enum('status',['ACTIVE','INACTIVE'])->default('ACTIVE')->nullable();
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channels');
+    }
+}
