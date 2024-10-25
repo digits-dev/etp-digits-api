53795da9baa2649e22f367081b6ff7df40579dbe
Mike Rodelas <mikerodelas@digits.ph>
2024-10-25 10:44:27 +0800

    add comment module

---
+++ database/migrations/2024_10_24_001007_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_pullouts_id')->nullable()->index();
+            $table->unsignedBigInteger('store_transfers_id')->nullable()->index();
+            $table->text('comment')->nullable();
+            $table->unsignedInteger('created_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
