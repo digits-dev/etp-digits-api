4b5be75659f992f0c1bea168ff1623c46cf0a9e0
Mike Rodelas <mikerodelas@digits.ph>
2024-11-06 10:48:33 +0800

    adjust user management module

---
+++ public/js/user.js
@@ -0,0 +1,12 @@
+const noChannelPriv = [1, 3, 4];
+
+$("#id_cms_privileges").change(function() {
+    const selectedPriv = parseInt($(this).val(), 10);
+    const isNoChannelPriv = noChannelPriv.includes(selectedPriv);
+    $("#store_masters_id, #channels_id").prop("required", !isNoChannelPriv);
+
+    $("#form-group-channels_id .control-label .text-danger").toggle(!isNoChannelPriv);
+    $("#form-group-store_masters_id .control-label .text-danger").toggle(!isNoChannelPriv);
+});
+
+$("#id_cms_privileges").trigger("change");
